96142fd03c1b64ef8b79ac6a00cd7e4337613e52
elxp101t3r <eliotx10101@outlook.com>
2023-10-14 20:36:30 +0300

    New position

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 #direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input('Type the shift number:\n'))
-def encrypt(text):
+def encrypt(text,shift):
+    #letters position
     x = []
     for i in range(len(text)):
         x.append(alphabet.index(text[i]))
-    print(x)
-encrypt(text)
+    #new position
+    c = []
+    for i in range(len(x)):
+        c.append(x[i]+shift)
+    print(c)
+encrypt(text,shift)
