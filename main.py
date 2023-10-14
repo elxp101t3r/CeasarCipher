e155860905f0a71875962f03f5aaac455021b460
elxp101t3r <eliotx10101@outlook.com>
2023-10-14 20:33:15 +0300

    The position of letters

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 #direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input('Type the shift number:\n'))
+def encrypt(text):
+    x = []
+    for i in range(len(text)):
+        x.append(alphabet.index(text[i]))
+    print(x)
+encrypt(text)
