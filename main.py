bdfd52f274648d248f4863168fb4f7196725efa7
elxp101t3r <eliotx10101@outlook.com>
2023-10-14 22:04:38 +0300

    Combine the 2

--- main.py
+++ main.py
@@ -2,23 +2,34 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input('Type the shift number:\n'))
-def encrypt(text,shift):
-    x = []
-    for i in range(len(text)):
-        x.append(alphabet.index(text[i]))
-    en_word = ''
-    for i in range(len(x)):
-        en_word += alphabet[x[i]+shift]
-    print(f'The encoded text is: {en_word}')
-def decrypt(text,shift):
-    x = []
-    for i in range(len(text)):
-        x.append(alphabet.index(text[i]))
-    d_word = ''
-    for i in range(len(x)):
-        d_word += alphabet[x[i]-shift]
-    print(f'The decoded text is: {d_word}')
-if direction == 'encode':
-    encrypt(text,shift)
-elif direction == 'decode':
-    decrypt(text,shift)
+# def encrypt(text,shift):
+#     x = []
+#     for i in range(len(text)):
+#         x.append(alphabet.index(text[i]))
+#     en_word = ''
+#     for i in range(len(x)):
+#         en_word += alphabet[x[i]+shift]
+#     print(f'The encoded text is: {en_word}')
+# def decrypt(text,shift):
+#     x = []
+#     for i in range(len(text)):
+#         x.append(alphabet.index(text[i]))
+#     d_word = ''
+#     for i in range(len(x)):
+#         d_word += alphabet[x[i]-shift]
+#     print(f'The decoded text is: {d_word}')
+# if direction == 'encode':
+#     encrypt(text,shift)
+# elif direction == 'decode':
+#     decrypt(text,shift)
+def caesar(plain_text, shift_amount, direction_of_user):
+    cipher_text = ''
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        if direction_of_user == 'encode':
+            new_position = position + shift_amount
+        elif direction_of_user == 'decode':
+            new_position = position - shift_amount
+        cipher_text += alphabet[new_position]
+    print(f'The encoded text is {cipher_text}')
+caesar(plain_text=text,shift_amount=shift,direction_of_user=direction)
