fcd6cdff42375128dedf37c280dfa6bc03322dda
elxp101t3r <eliotx10101@outlook.com>
2023-10-17 08:59:15 +0300

    Re order the code to check other conditions

--- main.py
+++ main.py
@@ -40,10 +40,23 @@ def caesar(start_text,shift_amount,cipher_direction):
     end_text = ''
     if cipher_direction == 'decode':
             shift_amount += -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = (position + shift_amount)%len(alphabet)
+            end_text += alphabet[new_position]
+        else:
+            if char in space:
+                position = space.index(char)
+                end_text += space[position]
+            if char in symbols:
+                position = symbols.index(char)
+                end_text += symbols[position]
+            if char in numbers:
+                position = numbers.index(char)
+                end_text += numbers[position]
+            else:
+                end_text += char
     print(f'The {cipher_direction}d text is {end_text}')
 
 caesar(start_text=text,shift_amount=shift,cipher_direction=direction)
